9337e14fb9c6387ee82b3e61e4e560a4022c4f54
Matthias Dellinger <thiasgaming@outlook.de>
2026-01-26 14:59:25 +0100

    created a function to serialize animals

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,23 +5,33 @@ def load_data(file_path):
   with open(file_path, "r", encoding="utf-8") as handle:
     return json.load(handle)
 
+
+def serialize_animal(animal_obj):
+    name = animal_obj.get("name", "Unknown Animal")
+    diet = animal_obj["characteristics"].get("diet", "Unknown")
+    location = animal_obj["locations"][0] if animal_obj.get("locations") else "Unknown"
+    animal_type = animal_obj["characteristics"].get("type")
+
+    output = f'<li class="cards__item">\n'
+    output += f'  <div class="card__title">{name}</div>\n'
+    output += f'  <p class="card__text">\n'
+    output += f'    <strong>Diet:</strong> {diet}<br/>\n'
+    output += f'    <strong>Location:</strong> {location}<br/>\n'
+
+    if animal_type:
+        output += f'    <strong>Type:</strong> {animal_type}<br/>\n'
+
+    output += '  </p>\n'
+    output += '</li>'
+
+    return output
+
+
 animals_data = load_data('animals_data.json')
 
 output = ""
 for animal_data in animals_data:
-    output += '<li class="cards__item">'
-    output += f'<div class="card__title">{animal_data["name"]}</div>\n'
-    output += f'<p class="card__text">'
-    output += f'<strong>Diet:</strong> {animal_data["characteristics"]["diet"]}<br/>\n'
-    output += f'<strong>Location:</strong> {animal_data["locations"][0]}<br/>\n'
-    try:
-        output += f'<strong>Type:</strong> {animal_data["characteristics"]["type"]}<br/>\n'
-    except:
-        output += '</p>'
-        output += '</li>'
-        continue
-    output += '</p>'
-    output += '</li>'
+    output += serialize_animal(animal_data)
 
 with open("animals_template.html", "r", encoding="utf-8") as file:
     html_inhalt = file.read()
