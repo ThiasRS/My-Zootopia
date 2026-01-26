bed92409bf162ca7f99b5d1d4fa1d22ced1629d1
Matthias Dellinger <thiasgaming@outlook.de>
2026-01-26 11:49:04 +0100

    print name, diet, location, type from every animal in json file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,4 +7,14 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
+for animal_data in animals_data:
+    print(f"Name: {animal_data['name']}")
+    print(f"Diet: {animal_data['characteristics']['diet']}")
+    print(f"Location: {animal_data['locations'][0]}")
+    try:
+        print(f"Type: {animal_data['characteristics']['type']}")
+    except:
+        print("")
+        continue
+    print("")
 
