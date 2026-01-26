f4808ea4095616142f3b2602b66a3eba8ac52155
Matthias Dellinger <thiasgaming@outlook.de>
2026-01-26 12:31:38 +0100

    improved html card elements

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,16 +10,19 @@ animals_data = load_data('animals_data.json')
 output = ""
 for animal_data in animals_data:
     output += '<li class="cards_item">'
-    output += f'Name: {animal_data["name"]}<br/>\n'
-    output += f"Diet: {animal_data['characteristics']['diet']}<br/>\n"
-    output += f"Location: {animal_data['locations'][0]}<br/>\n"
+    output += f'<div class="card__title">{animal_data["name"]}</div><br/>\n'
+    output += f'<p class="card_text">'
+    output += f'<strong>Diet:</strong> {animal_data["characteristics"]["diet"]}<br/>\n'
+    output += f'<strong>Location:</strong> {animal_data["locations"][0]}<br/>\n'
     try:
-        output += f"Type: {animal_data['characteristics']['type']}<br/>\n"
+        output += f'<strong>Type:</strong> {animal_data["characteristics"]["type"]}<br/>\n'
     except:
         output += "<br/>\n"
+        output += '</p>'
         output += '</li>'
         continue
     output += "<br/>\n"
+    output += '</p>'
     output += '</li>'
 
 with open("animals_template.html", "r", encoding="utf-8") as file:
