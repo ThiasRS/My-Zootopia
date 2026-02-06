81eb85a854213a44edc76f976411aa45cd5e6355
ThiasRS <thiasgaming@outlook.de>
2026-02-06 11:26:54 +0100

    Moved the main function to a seperated main file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -36,34 +36,3 @@ def user_picks_animal():
     return animal_name
 
 
-
-
-def main():
-    """Takes the data and creates the animals.html"""
-    animal_name = user_picks_animal()
-
-    animals_data = data_fetcher.fetch_data(animal_name)
-
-
-    output = ""
-    if not animals_data:
-        output = f'<h2 style="text-align: center;">The animal {animal_name} does not exist.</h2>'
-    else:
-        for animal_data in animals_data:
-            output += serialize_animal(animal_data)
-
-    with open("animals_template.html", "r", encoding="utf-8") as file:
-        html_inhalt = file.read()
-
-    animals_html = html_inhalt.replace("__REPLACE_ANIMALS_INFO__", output)
-
-    with open("animals.html", "w", encoding="utf-8") as file:
-        file.write(animals_html)
-
-    print("Website was successfully generated to the file animals.html")
-
-
-if __name__ == "__main__":
-    main()
-
-
---
+++ main.py
@@ -0,0 +1,30 @@
+import animals_web_generator
+import data_fetcher
+
+def main():
+    """Takes the data and creates the animals.html"""
+    animal_name = animals_web_generator.user_picks_animal()
+
+    animals_data = data_fetcher.fetch_data(animal_name)
+
+
+    output = ""
+    if not animals_data:
+        output = f'<h2 style="text-align: center;">The animal {animal_name} does not exist.</h2>'
+    else:
+        for animal_data in animals_data:
+            output += animals_web_generator.serialize_animal(animal_data)
+
+    with open("animals_template.html", "r", encoding="utf-8") as file:
+        html_inhalt = file.read()
+
+    animals_html = html_inhalt.replace("__REPLACE_ANIMALS_INFO__", output)
+
+    with open("animals.html", "w", encoding="utf-8") as file:
+        file.write(animals_html)
+
+    print("Website was successfully generated to the file animals.html")
+
+
+if __name__ == "__main__":
+    main()
